dea466639bf4201fabccddee1898b8aec1120ba4
RaimundoMonzon <acari021@outlook.com>
2024-10-15 11:56:42 -0300

    Index

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// So it begins
+require("dotenv").config();
+// El archivo .example.env es para que los estudiantes tengan un ejemplo de como recrearlo en su máquina local. Recordar que el archivo debe llamarse '.env' para que pueda ser levantado por el renglón anterior (y entonces Git dejará de versionarlo por lo que pusimos en .gitignore)
+
+const Server = require("./Server");
+
+const elServer = new Server();
+elServer.listen();
